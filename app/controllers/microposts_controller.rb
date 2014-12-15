89884db57991ba322060ea8609800affd962c60e
Tukubaev Maksat <tukubaevm094@gmail.com>
2014-12-15 09:51:01 +0000

    finish project

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,18 +1,19 @@
 class MicropostsController < ApplicationController
-  before_action :signed_in_user
-def create
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      @feed_items = []
       render 'static_pages/home'
     end
   end
-
-  def destroy
-     @micropost.destroy
+  
+ def destroy
+    @micropost.destroy
     flash[:success] = "Micropost deleted"
     redirect_to request.referrer || root_url
   end
@@ -21,5 +22,10 @@ def create
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,29 @@ class User < ActiveRecord::Base
   
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: :follower_id, dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: :followed_id, dependent: :destroy, class_name: "Relationship"
+  has_many :followers, through: :reverse_relationships, source: :follower
   
   before_save { self.email = self.email.downcase }
   before_create :create_remember_token
   
+  def follow!(other_user)
+    relationships.create(followed_id: other_user.id)
+  end
+  
+    def unfollow!(other_user)
+      relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  def following?(other_user)
+    relationships.find_by( followed_id: other_user.id)
+  end
+  
   def feed
-  microposts
-end
+   Micropost.from_users_followed_by(self)
+  end
   
   def User.new_remember_token
     SecureRandom.urlsafe_base64
